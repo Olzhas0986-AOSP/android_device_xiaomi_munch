43005bae78c19456debf140d38797d8cfaa29668
olzhas0986 <kdiroljas@gmail.com>
2026-08-13 01:45:54 +0500

    Munch: Remove extra backslash
    Signed-off-by: olzhas0986 <kdiroljas@gmail.com>

--- device.mk
+++ device.mk
@@ -110,7 +110,7 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     audio.primary.kona \
     audio.r_submix.default \
-    audio.usbv2.default \
+    audio.usbv2.default
 
 PRODUCT_PACKAGES += \
     libaudiopreprocessing \
